081b717a1f0126dbcbe44bad204ad9aa2448e3ca
Dev Ttangkong <ttankkeo112@gmail.com>
2025-02-23 08:51:47 +0900

    Added exmaple files and docs

---
+++ example/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rebuildable/widgets/rebuildable_app.dart';
+
+bool isDark = false;
+
+void main() {
+  runApp(const TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RebuildableApp(
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            theme: isDark ? ThemeData.dark() : ThemeData.light(),
+            home: Scaffold(
+              body: TestPage()
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Wrap(
+        children: [
+          TextButton(
+            onPressed: () {
+              isDark = false;
+              RebuildableApp.rebuild(); // like this
+            },
+            child: Text("Light"),
+          ),
+          TextButton(
+            onPressed: () {
+              isDark = true;
+              RebuildableApp.rebuild(); // like this
+            },
+            child: Text("Dark"),
+          )
+        ],
+      ),
+    );
+  }
+}
